9765a7808818db120aab8ff9a067be16c2c5f0b4
santospanda111 <santospanda111@gmail.com>
2021-05-08 10:04:23 +0530

    Added UC3 EmailId check code

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.regex.*;
 public class UserRegistration {
     private boolean check;
-    private String firstName,lastName;
+    private String firstName,lastName,email;
     private static final Scanner SCANNER  = new Scanner(System.in);
 /**
  * This checkFirstname method will check the user input according to the pattern.
@@ -37,9 +37,26 @@ public class UserRegistration {
             System.out.println(" It's a valid Lastname ");
     }
 
+    /**
+     * Added checkEmail method to match the email ids according to the given pattern.
+     */
+    public void checkEmail() {
+        System.out.println("Enter a valid email(Eg. abc.xyz@bl.co.in) : ");
+        email = SCANNER.nextLine();
+        check = Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
+        if (!check) {
+            System.out.println(" Invalid!!Email-id...Please Enter a Valid one. ");
+            checkEmail();
+        }
+        else {
+            System.out.println(" Email-Id is valid ");
+        }
+    }
+
     public static void main(String[]args) {
         UserRegistration user = new UserRegistration();
         user.checkFirstName();
         user.checkLastName();
+        user.checkEmail();
     }
 }
