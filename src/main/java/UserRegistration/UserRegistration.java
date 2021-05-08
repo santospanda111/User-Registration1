88ba2a7b14792f7f1840506e7ec8937942bba5d2
santospanda111 <santospanda111@gmail.com>
2021-05-08 09:50:53 +0530

    Added UC1 firstname check code

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -1,4 +1,29 @@
 package UserRegistration;
-
+import java.util.Scanner;
+import java.util.regex.*;
 public class UserRegistration {
+    private boolean check;
+    private String firstName;
+    private static final Scanner SCANNER  = new Scanner(System.in);
+/**
+ * This checkFirstname method will check the user input according to the pattern.
+ * for this operation i have imported regex module and Scanner module.
+ */
+    public void checkFirstName() {
+        System.out.println("Enter First-name starts with capital letter and has minimum 3 character : ");
+        firstName = SCANNER.nextLine();
+        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
+        if (!check) {
+            System.out.println("Invalid!!Please Enter your name according to the instruction.");
+            checkFirstName();
+        }
+        else {
+            System.out.println(" It's a Valid Firstname ");
+        }
+    }
+
+    public static void main(String[]args) {
+        UserRegistration user = new UserRegistration();
+        user.checkFirstName();
+    }
 }
