bb876a2161fbb2ea25ea560c36d8de10bc5466f2
santospanda111 <santospanda111@gmail.com>
2021-05-08 10:42:44 +0530

    Added UC8 Password check(Have 8chars,1uppercase,1special char,number) code

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -71,15 +71,17 @@ public class UserRegistration {
 
     /**
      * Password must be of 8 characters.
+     * Have one Uppercase Letter.
      * Have a Special Character(Eg-@$^)"
      * Must Have a Number.
      */
     public void checkPassword() {
         System.out.println("Enter Password(min 8 characters): ");
+        System.out.println("*Must have one Uppercase Letter(A-Z)");
         System.out.println("*Have a Special Character(Eg-@$^)");
         System.out.println("*Have a Number(0-9)");
         password = SCANNER.nextLine();
-        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
+        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]?){8,}.*$").matcher(password).matches();
         if (!check) {
             System.out.println(" Invalid Password!! Enter a valid one. ");
             checkPassword();
