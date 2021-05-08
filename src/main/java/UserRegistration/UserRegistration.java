6314ce52cd819530c7f496ddec703def9dd71774
santospanda111 <santospanda111@gmail.com>
2021-05-08 10:31:41 +0530

    Added UC7 Password check(Have a Number) code

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -72,12 +72,14 @@ public class UserRegistration {
     /**
      * Password must be of 8 characters.
      * Have a Special Character(Eg-@$^)"
+     * Must Have a Number.
      */
     public void checkPassword() {
         System.out.println("Enter Password(min 8 characters): ");
         System.out.println("*Have a Special Character(Eg-@$^)");
+        System.out.println("*Have a Number(0-9)");
         password = SCANNER.nextLine();
-        check = Pattern.compile("\"^(?=.*[A-Z])(?=.*[a-z]){8,}.*$\"").matcher(password).matches();
+        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
         if (!check) {
             System.out.println(" Invalid Password!! Enter a valid one. ");
             checkPassword();
