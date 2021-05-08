f2bab4055fb38a0fad7f43f39cbd1586277820f6
santospanda111 <santospanda111@gmail.com>
2021-05-08 09:55:59 +0530

    Added UC2 Lastname check code

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.regex.*;
 public class UserRegistration {
     private boolean check;
-    private String firstName;
+    private String firstName,lastName;
     private static final Scanner SCANNER  = new Scanner(System.in);
 /**
  * This checkFirstname method will check the user input according to the pattern.
@@ -22,8 +22,24 @@ public class UserRegistration {
         }
     }
 
+    /**
+     * This checkLastname method will check the user input according to the pattern.
+     */
+    public void checkLastName() {
+        System.out.println("Enter Lastname starts with capital letter and has minimum 3 character : ");
+        lastName = SCANNER.nextLine();
+        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+        if (!check) {
+            System.out.println("Invalid!!Please Enter your name according to the instruction.");
+            checkLastName();
+        }
+        else
+            System.out.println(" It's a valid Lastname ");
+    }
+
     public static void main(String[]args) {
         UserRegistration user = new UserRegistration();
         user.checkFirstName();
+        user.checkLastName();
     }
 }
