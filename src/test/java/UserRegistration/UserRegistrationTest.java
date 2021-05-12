4783b87642eedff888bfa9f77690edb7310d6cc0
santospanda111 <santospanda111@gmail.com>
2021-05-12 21:45:19 +0530

    Merged with UC12 successfully

--- src/test/java/UserRegistration/UserRegistrationTest.java
+++ src/test/java/UserRegistration/UserRegistrationTest.java
@@ -1,41 +1,53 @@
 package UserRegistration;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * here i have tested possible test cases with custom exception.
+ * printStackTrace method prints a stack trace for this Throwable object on the error output stream that is the value of the field System.err.
+ */
 public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator = new UserRegistration();
-        boolean result = UserRegistration.checkFirstName("Santosh");
-        Assertions.assertTrue(result);
+        try {
+            UserRegistration.checkFirstName("santosh");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator = new UserRegistration();
-        boolean result = UserRegistration.checkLastName("Santosh");
-        Assertions.assertTrue(result);
+        try {
+            UserRegistration.checkLastName("Panda");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenEmail_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator = new UserRegistration();
-        boolean result = UserRegistration.checkEmail("santospanda111@gmail.com");
-        Assertions.assertTrue(result);
+        try {
+            UserRegistration.checkEmail("abc-100@yahoo.com");
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
 
     }
 
     @Test
     public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator = new UserRegistration();
-        boolean result = UserRegistration.checkPhoneNumber("91 7978715564");
-        Assertions.assertTrue(result);
+        try {
+            UserRegistration.checkPhoneNumber("91 7978715564");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     @Test
     public void givenPassword_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator = new UserRegistration();
-        boolean result = UserRegistration.checkPassword("Santoshkumar1234@");
-        Assertions.assertTrue(result);
+        try {
+            UserRegistration.checkPassword("Santoshkumar1234@");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
 }
