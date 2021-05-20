3af9795026a5cf9fba92978aeb6f5fb0b4e0ce18
santospanda111 <santospanda111@gmail.com>
2021-05-20 21:08:11 +0530

    refactored

--- src/test/java/UserRegistration/UserRegistrationTest.java
+++ src/test/java/UserRegistration/UserRegistrationTest.java
@@ -4,12 +4,21 @@ import org.junit.jupiter.api.Test;
 /**
  * here i have tested possible test cases with custom exception.
  * printStackTrace method prints a stack trace for this Throwable object on the error output stream that is the value of the field System.err.
+ * Added negative test cases.
  */
 public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         try {
-            UserRegistration.checkFirstName("santosh");
+            UserRegistration.checkFirstName("Santosh");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        try {
+            UserRegistration.checkFirstName("san");
         }catch (UserRegistrationException e){
             e.printStackTrace();
         }
@@ -23,6 +32,14 @@ public class UserRegistrationTest {
             e.printStackTrace();
         }
     }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        try {
+            UserRegistration.checkLastName("pa");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void givenEmail_WhenProper_ShouldReturnTrue() {
@@ -31,7 +48,14 @@ public class UserRegistrationTest {
         }catch (IllegalArgumentException e){
             e.printStackTrace();
         }
-
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+        try {
+            UserRegistration.checkEmail("abcyahoo.");
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -43,9 +67,26 @@ public class UserRegistrationTest {
         }
     }
     @Test
+    public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
+        try {
+            UserRegistration.checkPhoneNumber("78715564");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void givenPassword_WhenProper_ShouldReturnTrue() {
         try {
             UserRegistration.checkPassword("Santoshkumar1234@");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnTrue() {
+        try {
+            UserRegistration.checkPassword("santoshku12");
         }catch (UserRegistrationException e){
             e.printStackTrace();
         }
